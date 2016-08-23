35da67d8ce1ba3ab257e634ced258f69581c8b89
William Sanders <william.sanders.dev@gmail.com>
2016-08-22 22:14:18 -0400

    added view and controller

--- src/Core-practice-website/Startup.cs
+++ src/Core-practice-website/Startup.cs
@@ -16,13 +16,22 @@ namespace Core_practice_website
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseDefaultFiles();
             app.UseStaticFiles();
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "App", action = "Index" }
+                    );
+            });
         }
     }
 }
