fdf2e9d6d44844b6f7cdbb79f3497757c6e3d6f8
William Sanders <william.sanders.dev@gmail.com>
2016-08-23 22:01:53 -0400

    added views, mail service

--- src/Core-practice-website/Controllers/Web/AppController.cs
+++ src/Core-practice-website/Controllers/Web/AppController.cs
@@ -4,12 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Core_practice_website.ViewModels;
+using Core_practice_website.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace Core_practice_website.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (model.Email.Contains("aol.com"))
+            {
+                ModelState.AddModelError("", "We don't support AOL addresses.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From Practice Cite", model.Message);
+
+                ModelState.Clear();
+
+                ViewBag.UserMessage = "Message Sent";
+            }
+            return View();
+        }
+
+        public IActionResult About()
         {
             return View();
         }
---
+++ src/Core-practice-website/Services/IMailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_practice_website.Services
+{
+    public interface IMailService
+    {
+        void SendMail(string to, string from, string subject, string body);
+    }
+}
