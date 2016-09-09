ad5418bbb1499ac16ec09f44e3abf1d3642f2e1f
William Sanders <william.sanders.dev@gmail.com>
2016-09-08 23:14:03 -0400

    added GMap

--- src/Core-practice-website/wwwroot/js/tripEditorController.js
+++ src/Core-practice-website/wwwroot/js/tripEditorController.js
@@ -16,6 +16,7 @@
             .then(function (response) {
                 //success
                 angular.copy(response.data, vm.stops);
+                _showMap(vm.stops);
             }, function (err) {
                 //failure
                 vm.errorMessage = "Failed to load stops";
@@ -24,4 +25,28 @@
                 vm.isBusy = false;
             });
     }
+
+    function _showMap(stops) {
+
+        if (stops && stops.length > 0) {
+
+            var mapStops = _.map(stops, function (item) {
+                return {
+                    lat: item.latitude,
+                    long: item.longitude,
+                    info: item.name
+                };
+            });
+
+            //Show map
+            travelMap.createMap({
+                stops: mapStops,
+                selector: "#map",
+                currentStop: 1,
+                initialZoom: 3
+            });
+        }
+
+    }
+
 })();
